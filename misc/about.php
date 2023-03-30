70a9259b72c773c6ec1cd790ffaef80d4c102eda
Edward Gilbert <egbot@asu.edu>
2023-03-30 12:50:49 -0700

    Merge DEMCA specific files

---
+++ misc/about.php
@@ -0,0 +1,34 @@
+<?php
+include_once("config/symbini.php");
+header("Content-Type: text/html; charset=".$CHARSET);
+?>
+<html>
+<head>
+	<meta http-equiv="X-Frame-Options" content="deny">
+	<title><?php echo $DEFAULT_TITLE; ?> Home</title>
+	<link href="css/base.css?<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
+	<link href="css/main.css?<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
+	<script type="text/javascript">
+		<?php include_once($SERVER_ROOT.'/config/googleanalytics.php'); ?>
+	</script>
+</head>
+<body>
+	<?php
+	include($SERVER_ROOT.'/header.php');
+	?> 
+	<!-- This is inner text! -->
+	<div  id="innertext">
+		<h1></h1>
+
+		
+			<b>About</b><br><br>
+			
+			Under development, text forthcoming
+		</div>
+	</div>
+
+	<?php
+	include($SERVER_ROOT.'/footer.php');
+	?> 
+</body>
+</html>
